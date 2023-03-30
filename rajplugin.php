367feb80a629424cb4ada64c1258d9939c445b2d
DR_GURUNG <racingfoxs@gmail.com>
2023-03-30 18:49:34 +0530

    Plugin Initilized

---
+++ rajplugin.php
@@ -0,0 +1,15 @@
+<?php
+
+/*
+Plugin Name: Raj Plugin
+Plugin URI: https://github.com/
+Description: Plugin Development.
+Version: 1.0
+Requires at least: 5.0
+Requires PHP: 5.2
+Author: Automattic
+Author URI: https://github.com/
+License: GPLv2 or later
+*/
+
+echo "<div class='notice'> Hello World </div>";
